0bf59919335e07b4758df613f70fe3bbb22a67be
TetianaKurylenko <58007314+TetianaKurylenko@users.noreply.github.com>
2019-11-26 22:24:51 -0700

    Add Drivers - Safari, Chrome, Firefox

--- src/test/java/LoginTestAdmin.java
+++ src/test/java/LoginTestAdmin.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,29 +13,39 @@ import java.time.Duration;
 
 public class LoginTestAdmin {
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private WebDriver[] driversArray;
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriver chromeDriver = new ChromeDriver();
+        WebDriver firefoxDriver = new FirefoxDriver();
+        WebDriver safariDriver = new SafariDriver();
+
+        driversArray = new WebDriver[] {chromeDriver, firefoxDriver, safariDriver};
     }
 
     @After
     public void stop() {
-        driver.quit();
-        driver = null;
+        for (int i = 0; i < driversArray.length; i++) {
+            driversArray[i].quit();
+        }
+        driversArray = null;
     }
 
     @Test
     public void loginTest() {
-        driver.get("http://192.168.64.2/litecart/admin/");
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.cssSelector("button[name='login'][type='submit'] ")).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Logout']")));
+        for (WebDriver driver : driversArray) {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            driver.get("http://192.168.64.2/litecart/admin/");
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.cssSelector("button[name='login'][type='submit'] ")).click();
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='Logout']")));
+        }
+
     }
 }
 
